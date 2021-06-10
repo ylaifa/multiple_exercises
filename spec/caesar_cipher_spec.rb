f47899cfce74495f9bc23b9e8096a555d2eb2b29
Yousri LAÏFA <laifayousri@gmail.com>
2021-06-10 13:44:44 +0200

    Add caesar_cipher_spec

---
+++ spec/caesar_cipher_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../lib/caesar_cipher'
+
+describe "letter_transpose method" do
+  it "Change the letter based on the number" do
+    expect(letter_transpose("A",3)).to eq("D")
+    expect(letter_transpose("B",5)).to eq("G")
+    expect(letter_transpose("v",10)).to eq("f")
+    expect(letter_transpose("w",3)).to eq("z")
+  end
+end
+
+describe "caesar_cipher method" do
+  it "return encryted string" do
+    expect(caesar_cipher("What a string!", 5)).to eq("Bmfy f xywnsl!")
+  end
+end
